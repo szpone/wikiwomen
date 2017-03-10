d6f71e24097841e5f2b1bda43243a2b7f5c7c4c1
Nikola Adamus <nikola.adamus@gmail.com>
2017-03-10 20:29:04 +0100

    forms

--- wikiwomen/scientists/forms.py
+++ wikiwomen/scientists/forms.py
@@ -4,3 +4,4 @@ from .models import Scientist
 class ChooseSpecializationForm(forms.Form):
     specialization = forms.ModelChoiceField(queryset=Scientist.objects.all(),
                                     required=True, empty_label="----")
+
