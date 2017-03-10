18fb01f7d18d464cba0b6f929cc7448e103f407b
Nikola Adamus <nikola.adamus@gmail.com>
2017-03-10 19:05:34 +0100

    models for scientists and specialization, add requirments

--- wikiwomen/scientists/models.py
+++ wikiwomen/scientists/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Scientists(models.Model):
+    name = models.CharField(max_length=128)
+    gender = models.CharField(max_lenght=64)
+    link = models.CharField(max_length=128)
+    extra_info = models.CharField(max_length=128, null=True)
+
+
+class Specialization(models.Model):
+    name = models.CharField(max_length=128)
+    scientists = models.ForeignKey(Scientists, on_delete=models.CASCADE)
