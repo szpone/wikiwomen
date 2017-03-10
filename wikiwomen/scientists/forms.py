ae3d0ff6004f7871556ea2acbc1f6fe760602551
Nikola Adamus <nikola.adamus@gmail.com>
2017-03-10 20:06:33 +0100

    templates

--- wikiwomen/scientists/forms.py
+++ wikiwomen/scientists/forms.py
@@ -1,3 +1,6 @@
 from django import forms
 from .models import Scientist
 
+class ChooseSpecializationForm(forms.Form):
+    specialization = forms.ModelChoiceField(queryset=Scientist.objects.all(),
+                                    required=True, empty_label="----")
--- wikiwomen/scientists/models.py
+++ wikiwomen/scientists/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-
 class Scientist(models.Model):
     women_number = models.IntegerField
     men_number = models.IntegerField
