817ff0867f2033107b827c99c0c7685a9aa9ae26
Nikola Adamus <nikola.adamus@gmail.com>
2017-03-10 20:35:50 +0100

    change models and view

--- wikiwomen/scientists/models.py
+++ wikiwomen/scientists/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Scientist(models.Model):
-    women_number = models.IntegerField(default=0)
-    men_number = models.IntegerField(default=0)
+    women = models.IntegerField(default=0)
+    men = models.IntegerField(default=0)
     specialization = models.CharField(max_length=128)
--- wikiwomen/scientists/views.py
+++ wikiwomen/scientists/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import FormView
+from .models import Scientist
+from .forms import ChooseSpecializationForm
 
 # Create your views here.
+
+class ChooseSpecialization(FormView):
+    template_name = 'scientists/index.html'
+    form_class = ChooseSpecializationForm
